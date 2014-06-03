fcbda1d0ebabbc1cc47a4d767c2cea92e4ada1f6
Hydrog3n <loic.vaille@gmail.com>
2014-06-03 23:10:07 +0200

    Passe en v1.0.0
    Merge de la branche dev

--- config.default.php
+++ config.default.php
@@ -3,14 +3,11 @@
 $name_site = "ShareFiles";
 // $url = $_SERVER['']; a faire
 
-// Configuration des fichiers 
+// Configuration des fichiers
 
 $max_size = 10; //taille en Mo
-$extend_files = array( '.zip', '.rar', '.dmg', '.tar.gz'); // Posibilité d'en rajouter
+$extend_files = array( '.zip', '.rar', '.dmg', '.tar.gz', '.docx', '.doc', '.xlsx', '.xls', '.pdf', '.odt'); // Posibilité d'en rajouter
 
 // pseudo et mot de pass pour l'administration
 $admin_pseudo = "admin";
 $admin_password = "admin";
-
-
-
